b89161253082c8f7089826a4debd65f114177acb
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-08-28 21:22:18 -0700

    start sessions controller spec

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe SessionController do
+	describe "POST create" do
+		context "with valid user" do
+			it "redirects to stories page"
+			it "sets session to user id"
+			it "displays flash success"
+		end
+	end
+end
