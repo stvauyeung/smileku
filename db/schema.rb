29eb886039177882a5aefc49a53312226e36afed
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-09-05 21:19:21 -0700

    remove signups

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20130625021022) do
 
-  create_table "signups", :force => true do |t|
-    t.string   "firstname"
-    t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "stories", :force => true do |t|
     t.string   "title"
     t.string   "body"
