8b78c4872f8e86b7a830462d0b9fd8e25b540b11
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-09-06 22:34:37 -0700

    add user spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
   has_many :stories
   has_many :kus
-
   has_secure_password
-  validates :password, :presence => true,  :on => :save
-
+  validates :password, :presence => true,  :on => :create
   validates :username, :presence => true, :uniqueness => true
   validates :email, :presence => true, :uniqueness => true
+
+  def has_secure_password?
+  	true
+  end
 end
