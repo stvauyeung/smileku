d342d9750264cdea1a115a113cbff624df42317e
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-09-13 00:56:08 -0700

    happy path for kuscontroller create

--- app/controllers/kus_controller.rb
+++ app/controllers/kus_controller.rb
@@ -5,6 +5,14 @@ class KusController < ApplicationController
 	end
 
 	def create
+		story = Story.find(params[:story_id])
+		ku = Ku.create(params[:ku])
+		ku.update_column(:story_id, story.id)
+		flash[:success] = "You successfully published a ku."
+		redirect_to story_ku_path(story, ku)
+	end
+
+	def show
 		
 	end
 end
--- spec/controllers/kus_controller_spec.rb
+++ spec/controllers/kus_controller_spec.rb
@@ -13,21 +13,33 @@ describe KusController do
 			get :new, story_id: story.id, parent_id: first_ku.id
 			expect(assigns[:ku].story_id).to eq(story.id)
 		end
-		it "sets the ku parent" do
-			get :new, story_id: story.id, parent_id: first_ku.id
-			expect(assigns[:ku].parent_id).to eq(first_ku.id)
-		end
 	end
 
 	describe "POST create" do
 		context "successful creation" do
-			it "creates a new ku" do
-
+			let(:story) { Fabricate(:story) }
+			let(:first_ku) { Fabricate(:ku, story_id: story.id) }
+			
+			it "redirects to new ku show page" do
+				post :create, ku: {parent_id: first_ku.id, body: "body"}, story_id: story.id
+				response.should redirect_to story_ku_path(story, Ku.find(2))
 			end
-			it "associates ku with parent"
-			it "associates ku with story"
-			it "redirects to ku page"
-			it "sets flash success"
+			it "creates a new ku" do
+				post :create, ku: {parent_id: first_ku.id, body: "body"}, story_id: story.id
+				expect(Ku.count).to eq(2)
+			end
+			it "associates ku with parent" do
+				post :create, ku: {parent_id: first_ku.id, body: "body"}, story_id: story.id
+				expect(Ku.last.parent_id).to eq(first_ku.id)
+			end
+			it "associates ku with story" do
+				post :create, ku: {parent_id: first_ku.id, body: "body"}, story_id: story.id
+				expect(Ku.last.story_id).to eq (story.id)
+			end
+			it "sets flash success" do
+				post :create, ku: {parent_id: first_ku.id, body: "body"}, story_id: story.id
+				expect(flash[:success]).to be_present
+			end
 		end
 		context "unsuccessful creation" do
 
