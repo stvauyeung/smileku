5bc884bc94a72dddf12d81773c08be2d09d0e552
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-09-13 17:38:58 -0700

    finish kus controller create

--- spec/controllers/kus_controller_spec.rb
+++ spec/controllers/kus_controller_spec.rb
@@ -4,7 +4,11 @@ describe KusController do
 	describe "GET new" do
 		let(:story) { Fabricate(:story) }
 		let(:first_ku) { Fabricate(:ku, story_id: story.id) }
+		before { sign_in_user }
 
+		it_behaves_like "require_login" do
+			let(:action) { get :new, story_id: story.id, parent_id: first_ku.id }
+		end
 		it "creates a new ku" do
 			get :new, story_id: story.id, parent_id: first_ku.id
 			expect(assigns[:ku]).to be_an_instance_of(Ku)
@@ -16,10 +20,15 @@ describe KusController do
 	end
 
 	describe "POST create" do
+		let(:bob) { Fabricate(:user) }
+		let(:story) { Fabricate(:story) }
+		let(:first_ku) { Fabricate(:ku, story_id: story.id) }
+		before { sign_in_user(bob) }
+
+		it_behaves_like "require_login" do
+			let(:action) { post :create, ku: {parent_id: first_ku.id, body: "body"}, story_id: story.id }
+		end
 		context "successful creation" do
-			let(:story) { Fabricate(:story) }
-			let(:first_ku) { Fabricate(:ku, story_id: story.id) }
-			
 			it "redirects to new ku show page" do
 				post :create, ku: {parent_id: first_ku.id, body: "body"}, story_id: story.id
 				response.should redirect_to story_ku_path(story, Ku.find(2))
@@ -27,6 +36,10 @@ describe KusController do
 			it "creates a new ku" do
 				post :create, ku: {parent_id: first_ku.id, body: "body"}, story_id: story.id
 				expect(Ku.count).to eq(2)
+			end
+			it "sets current user as ku user" do
+				post :create, ku: {parent_id: first_ku.id, body: "body"}, story_id: story.id
+				expect(Ku.last.user).to eq(bob)
 			end
 			it "associates ku with parent" do
 				post :create, ku: {parent_id: first_ku.id, body: "body"}, story_id: story.id
@@ -42,7 +55,18 @@ describe KusController do
 			end
 		end
 		context "unsuccessful creation" do
-
+			it "should not create a new ku" do
+				post :create, ku: {parent_id: first_ku.id, body: ""}, story_id: story.id
+				expect(Ku.count).to eq(1)
+			end
+			it "should render the new ku page" do
+				post :create, ku: {parent_id: first_ku.id, body: ""}, story_id: story.id
+				response.should render_template :new
+			end
+			it "should display flash error" do
+				post :create, ku: {parent_id: first_ku.id, body: ""}, story_id: story.id
+				expect(flash[:error]).to be_present
+			end
 		end
 	end
 end
