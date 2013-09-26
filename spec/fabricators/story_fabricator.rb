94aca814af56528fd175b600768ccea5863347a3
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-09-25 23:22:00 -0700

    debugging imagemagick

--- spec/fabricators/story_fabricator.rb
+++ spec/fabricators/story_fabricator.rb
@@ -1,7 +1,8 @@
 Fabricator(:story) do
 	title { Faker::Lorem.words(5).join(' ') }
 	description { Faker::Lorem.sentences(2).join(' ') }
-	before_create { |story| story.large_cover = File.open("public/tmp/mrec1.jpg", "w") }
+	large_cover { File.open("public/tmp/mrec1.jpg", "w") }
+	# before_create { |story| story.large_cover = File.open("public/tmp/mrec1.jpg", "w") }
 end
 
 Fabricator(:invalid_story, from: :story) do
