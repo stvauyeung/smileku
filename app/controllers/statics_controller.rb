eff83b73fe5e5ecb95e6c7aa5a7d3926caccda15
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-10-13 15:46:41 -0700

    create contact form

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,7 +1,22 @@
 class StaticsController < ApplicationController
-	layout 'front'
+	layout :resolve_layout
 
 	def front
 		@user = User.new
+	end
+
+	def contact
+		
+	end
+
+	private
+
+	def resolve_layout
+		case action_name
+		when "front"
+			"front"
+		else
+			"application"
+		end
 	end
 end
