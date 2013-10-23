6e6d881bad90c52becdb82729903df2b29f20cfe
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-10-22 22:39:48 -0700

    working on reset passwords controller spec

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,6 +4,16 @@ class PasswordResetsController < ApplicationController
 	end
 
 	def create
+		user = User.find_by_token(params[:token])
+		if user && params[:password].present?
+			user.password = params[:password]
+			user.generate_token
+			user.save!
+			flash[:success] = "You've successfully reset your password. Use your new password to login now."
+			redirect_to login_path
+		else
+			render :show
+		end
 		
 	end
 end
