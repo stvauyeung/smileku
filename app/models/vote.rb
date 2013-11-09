d97cbf5d8380c0f91ec70c508e0ba41a405b03cf
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-11-08 20:02:44 -0800

    refactor ku vote methods to vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,21 @@ class Vote < ActiveRecord::Base
 	belongs_to :ku
 	belongs_to :user
 	belongs_to :voteable, polymorphic: true
+
+	def self.for_ku(ku, user, value)
+		if ku.votes.where(user_id: user.id).exists?
+			handle_existing_vote(ku, user, value)
+		else
+			Vote.create(value: value, voteable_type: "Ku", voteable_id: ku.id, user_id: user.id)
+		end
+	end
+
+	def self.handle_existing_vote(ku, user, value)
+		vote = ku.votes.where(user_id: user.id).first
+		if vote.value.to_s == value
+			vote.destroy
+		else
+			vote.update_attributes(value: value)
+		end
+	end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,4 +3,24 @@ require 'spec_helper'
 describe Vote do
 	it { should belong_to(:voteable) }
 	it { should belong_to(:user) }
+
+	describe ".for_ku" do
+		let(:bob) { Fabricate(:user) }
+		let(:ku) { Fabricate(:ku) }
+
+		it "creates new vote if ku has no votes from user" do
+			Vote.for_ku(ku, bob, 'true')
+			expect(Vote.count).to eq(1)
+		end
+		it "removes vote if ku has vote from user of same value" do
+			Fabricate(:vote, user_id: bob.id)
+			Vote.for_ku(ku, bob, 'true')
+			expect(Vote.count).to eq(0)
+		end
+		it "replaces vote if ku has vote from user of opposite value" do
+			Fabricate(:vote, user_id: bob.id)
+			Vote.for_ku(ku, bob, 'false')
+			expect(Vote.last.value).to eq(false)
+		end
+	end
 end
