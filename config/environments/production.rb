42b366fca9dcad81efb4bd0465e4ffcbab589c78
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-12-01 16:14:10 -0800

    move icon png to asset images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,6 @@ FirstBook::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-  config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf' )
 
   # Generate digests for assets URLs
   config.assets.digest = true
