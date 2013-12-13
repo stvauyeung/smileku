2619e9191e3ea42f528d77068054ba7807496762
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-12-13 11:45:47 -0800

    followings controller done

--- spec/controllers/followings_controller_spec.rb
+++ spec/controllers/followings_controller_spec.rb
@@ -1,12 +1,50 @@
 require 'spec_helper'
+require 'pry'
 
 describe FollowingsController do
-	describe 'POST create' do
-		let(:bob) { Fabricate(:user) }
-		let(:sam) { Fabricate(:user) }
+	let(:bob) { Fabricate(:user) }
+	let(:sam) { Fabricate(:user) }
+	let(:joe) { Fabricate(:user) }
 
-		it_behaves_like "require_login" do
+	describe 'POST create' do
+		before { sign_in_user(bob) }
+
+		it_behaves_like 'require_login' do
 			let(:action) { post :create, following: { followed_id: sam.id } }
+		end
+
+		it 'creates following if no following bt users exists' do
+			post :create, following: { followed_id: sam.id }
+			expect(bob.followings.count).to eq(1)
+		end
+		it 'does not create following if following bt users exists' do
+			Fabricate(:following, follower_id: bob.id, followed_id: sam.id)
+			post :create, following: { followed_id: sam.id }
+			expect(bob.followings.count).to eq(1)
+		end
+		it 'it does not allower user to follow herself' do
+			post :create, following: { followed_id: bob.id }
+			expect(bob.followings.count).to eq(0)
+		end
+	end
+
+	describe 'DELETE destroy' do
+		let(:following) { Fabricate(:following, follower_id: bob.id, followed_id: sam.id) }
+		before { sign_in_user(bob) }
+
+		it_behaves_like 'require_login' do
+			let(:action) { delete :destroy, id: following.id }
+		end
+
+		it 'deletes following if current user is follower' do
+			delete :destroy, id: following.id
+			expect(bob.followings.count).to eq(0)
+		end
+
+		it 'does not delete following if current user not follower' do
+			sign_in_user(joe)
+			delete :destroy, id: following.id
+			expect(bob.followings.count).to eq(1)
 		end
 	end
 end
