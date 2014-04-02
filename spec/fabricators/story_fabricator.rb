a57185905724210897b147eec5e654bede3df282
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-04-01 22:02:06 -0700

    updated seed.rb and fabricators

--- spec/fabricators/story_fabricator.rb
+++ spec/fabricators/story_fabricator.rb
@@ -1,8 +1,10 @@
 Fabricator(:story) do
 	title { Faker::Lorem.words(5).join(' ') }
 	description { Faker::Lorem.sentences(2).join(' ') }
+	user_id { Fabricate(:user).id }
 	# large_cover { File.open("public/tmp/mrec1.jpg", "w") }
-	after_build { |story| story.large_cover = File.open("public/tmp/mrec1.jpg", "w") }
+	after_build { |story| story.large_cover = File.open("public/tmp/cat1.png") }
+	after_create { |story| Fabricate(:ku, user_id: story.user_id, story_id: story.id)}
 end
 
 Fabricator(:invalid_story, from: :story) do
